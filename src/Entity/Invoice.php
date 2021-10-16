68089fad8a9c62ba285f7ac0681c37f13783b2bb
aouhman <abdessamad.ouhman@gmail.com>
2021-10-16 19:30:09 +0000

    surcharge pagination

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=InvoiceRepository::class)
- * @ApiResource
+ * @ApiResource(attributes={"pagination_enabled=true"}
  */
 class Invoice
 {
